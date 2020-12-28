815e46cb75c341fd9e90fa3d6fc4569d1e22007c
nguyenthanh2901 <nguyenthanh2901hust@gmail.com>
2020-12-28 15:17:53 +0700

    first

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,114 @@
+<?php
+namespace AHT\Question\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, 
+    \Magento\Framework\Setup\ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        if (!$installer->tableExists('aht_question')) {
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('aht_question')
+            )
+                ->addColumn(
+                    'question_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'nullable' => false,
+                        'primary'  => true,
+                        'unsigned' => true,
+                    ],
+                    'QUESTION ID'
+                )
+                ->addColumn(
+                    'name',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    ['nullable' => false],
+                    'NAME'
+                )
+                ->addColumn(
+                    'email',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    ['nullable' => false],
+                    'EMAIL'
+                )
+                ->addColumn(
+                    'question',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    '64k',
+                    ['nullable' => false],
+                    'QUESTION'
+                )
+                ->addColumn(
+                    'answer',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    '64k',
+                    ['nullable' => false, 'default'=>''],
+                    'ANSWER'
+                )
+                ->addColumn(
+                    'status',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'STATUS'
+                )
+                ->addColumn(
+                    'product_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['nullable' =>false, 'default' => '-1'],
+                    'PRODUCT ID'
+                )
+                ->addColumn(
+                    'user_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['nullable' =>false, 'default' => '-1'],
+                    'USER ID'
+                )
+                ->addColumn(
+                    'store_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['nullable' =>false, 'default' => '-1'],
+                    'STORE ID'
+                )
+                ->addColumn(
+                    'created_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                    null,
+                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                    'Created At'
+                )->addColumn(
+                    'updated_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                    null,
+                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                    'Updated At'
+                )
+                ->setComment('Question Table');
+            $installer->getConnection()->createTable($table);
+
+            $installer->getConnection()->addIndex(
+                $installer->getTable('aht_question'),
+                $setup->getIdxName(
+                    $installer->getTable('aht_question'),
+                    ['name', 'email', 'question'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+                ),
+                ['name', 'email', 'question'],
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+        }
+        $installer->endSetup();
+    }
+}
